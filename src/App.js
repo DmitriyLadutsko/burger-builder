15cdb6dff7d0281a830c2ec4b33bf7d3cf4874f6
Dmitriy Ladutsko <dima.ladut@gmail.com>
2020-10-25 23:56:45 +0300

    Creating a Layout Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Layout from './components/Layout'
 
 function App() {
   return (
     <div >
-      <header >
-        <h1>Hello!!</h1>
-      </header>
+      <Layout>
+          <p>Test</p>
+      </Layout>
     </div>
   );
 }
