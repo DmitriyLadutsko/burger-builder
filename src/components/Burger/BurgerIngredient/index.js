b1a6d5d27c129ad0775409b0a2e702c1abcf9efb
Dmitriy Ladutsko <dima.ladut@gmail.com>
2020-10-27 11:34:03 +0300

    Starting the Burger Component

--- src/components/Burger/BurgerIngredient/index.js
+++ src/components/Burger/BurgerIngredient/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import classes from './BurgerIngredient.css';
+import classes from './BurgerIngredient.module.css';
 import PropTypes from 'prop-types';
 
 const burgerIngredient = (props) => {
@@ -7,27 +7,27 @@ const burgerIngredient = (props) => {
 
     switch (props.type) {
         case ('bread-bottom'):
-            ingredient = <div className={classes.BreadBottom}></div>;
+            ingredient = <div className={classes.BreadBottom}/>;
             break;
         case ('bread-top'):
             ingredient = (
                 <div className={classes.BreadTop}>
-                    <div className={classes.Seeds1}></div>
-                    <div className={classes.Seeds2}></div>
+                    <div className={classes.Seeds1}/>
+                    <div className={classes.Seeds2}/>
                 </div>
             );
             break;
         case ('meat'):
-            ingredient = <div className={classes.Meat}></div>;
+            ingredient = <div className={classes.Meat}/>;
             break;
         case ('bacon'):
-            ingredient = <div className={classes.Bacon}></div>;
+            ingredient = <div className={classes.Bacon}/>;
             break;
         case ('salad'):
-            ingredient = <div className={classes.Salad}></div>;
+            ingredient = <div className={classes.Salad}/>;
             break;
         case ('cheese'):
-            ingredient = <div className={classes.Cheese}></div>;
+            ingredient = <div className={classes.Cheese}/>;
             break;
         default:
             ingredient = null;
