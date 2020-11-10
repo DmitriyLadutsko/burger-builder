d64abd7aed4d62139ebccf6898c8b30ea7b9bfcc
Dmitriy Ladutsko <dima.ladut@gmail.com>
2020-11-10 12:28:08 +0300

    Testing

--- src/index.js
+++ src/index.js
@@ -20,9 +20,7 @@ const rootReducer = combineReducers({
     auth: authReducer
 });
 
-const store = createStore(rootReducer, composeEnhancers(
-    applyMiddleware(thunk)
-));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
     <Provider store={store}>
